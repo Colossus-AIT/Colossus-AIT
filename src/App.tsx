c982936f8a036217e8d8b8e9fdebf651f87c0cff
Colossus-ISE <colossusise@gmail.com>
2026-03-22 10:01:33 +0530

    Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-
+import { Analytics } from "@vercel/analytics/react";
 
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -30,7 +30,6 @@ const ScrollToHash = () => {
   return null;
 };
 
-
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
@@ -40,7 +39,6 @@ const ScrollToTop = () => {
 
   return null;
 };
-
 
 const App = () => {
   const [isDark, setIsDark] = useState(true);
@@ -70,9 +68,9 @@ const App = () => {
         <Toaster />
         <Sonner />
         <BrowserRouter>
-         <ScrollToTop />
+          <ScrollToTop />
           <ScrollToHash />
-
+          <Analytics />
           <div className="relative min-h-screen">
             {/* Global Navigation */}
             <Navigation onThemeToggle={toggleTheme} isDark={isDark} />
